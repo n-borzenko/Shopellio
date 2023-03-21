c6cf521eabef70181ee3daec047ac27f51b9b1fe
Natalia Borzenko <n.borzenko93@gmail.com>
2023-03-21 01:02:08 +0000

    added image urls for test data

--- Borzenko-Shopellio/Models/SampleData.swift
+++ Borzenko-Shopellio/Models/SampleData.swift
@@ -25,7 +25,8 @@ enum SampleData {
       "steel blue": "#4682B4",
       "baby pink": "#F4C2C2",
       "burgundy": "#800020",
-      "brown": "#964B00"
+      "brown": "#964B00",
+      "silver": "#C0C0C0"
     ],
     sizes: ["One size", "XS", "S", "M", "L", "XL", "XXL", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
     categories: [
@@ -67,7 +68,7 @@ enum SampleData {
     ),
     Product(
       title: "Faux leather oversize jacket",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1511280303142-0051e93baeeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80"],
       subcategoryId: "coats",
       price: 95.0,
       discount: 0.05,
@@ -81,7 +82,7 @@ enum SampleData {
     ),
     Product(
       title: "Mini dress with floral print",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1617383619176-593fe8bfcde4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=747&q=80"],
       subcategoryId: "dresses",
       price: 32.0,
       discount: 0.0,
@@ -99,37 +100,39 @@ enum SampleData {
     ),
     Product(
       title: "Puff shoulder pencil dress",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1582533575066-75bd83ac91de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"],
       subcategoryId: "dresses",
       price: 55.0,
       discount: 0.2,
       stock: [
+        StockItem(variant: ProductVariant(color: "silver", size: "S"), level: .low),
+        StockItem(variant: ProductVariant(color: "silver", size: "M"), level: .low),
         StockItem(variant: ProductVariant(color: "navy blue", size: "S"), level: .low),
         StockItem(variant: ProductVariant(color: "navy blue", size: "L"), level: .low)
       ],
       sizes: ["S", "M", "L"],
-      colors: ["navy blue"]
+      colors: ["silver", "navy blue"]
     ),
     Product(
       title: "Multi pocket cargo trouser",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1594633312681-425c7b97ccd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"],
       subcategoryId: "trousers",
       price: 35.0,
       discount: 0.0,
       stock: [
         StockItem(variant: ProductVariant(color: "black", size: "S"), level: .low),
-        StockItem(variant: ProductVariant(color: "beige", size: "M"), level: .normal),
+        StockItem(variant: ProductVariant(color: "baby pink", size: "M"), level: .normal),
         StockItem(variant: ProductVariant(color: "black", size: "M"), level: .normal),
-        StockItem(variant: ProductVariant(color: "beige", size: "L"), level: .normal),
+        StockItem(variant: ProductVariant(color: "baby pink", size: "L"), level: .normal),
         StockItem(variant: ProductVariant(color: "black", size: "L"), level: .low)
       ],
       sizes: ["S", "M", "L"],
-      colors: ["beige", "black"],
+      colors: ["baby pink", "black"],
       tags: ["new"]
     ),
     Product(
       title: "Black trainers for running",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1547586877-0351a7143cbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"],
       subcategoryId: "w_shoes",
       price: 75.0,
       discount: 0.0,
@@ -145,7 +148,7 @@ enum SampleData {
     ),
     Product(
       title: "Regular fit suit",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1585846416120-3a7354ed7d39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"],
       subcategoryId: "suits",
       price: 129.0,
       discount: 0.0,
@@ -164,7 +167,7 @@ enum SampleData {
     ),
     Product(
       title: "Regular fit round-neck t-shirt",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"],
       subcategoryId: "t-shirts",
       price: 15.0,
       discount: 0.05,
@@ -198,7 +201,7 @@ enum SampleData {
     ),
     Product(
       title: "Basic T-shirt",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"],
       subcategoryId: "t-shirts",
       price: 15.0,
       discount: 0.0,
@@ -229,7 +232,7 @@ enum SampleData {
     ),
     Product(
       title: "Printed t-shirt with pocket",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1613243706336-150bf7ec02f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"],
       subcategoryId: "t-shirts",
       price: 18.0,
       discount: 0.0,
@@ -249,7 +252,7 @@ enum SampleData {
     ),
     Product(
       title: "Satin wrap blouse",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1655203091785-9b07e64e4459?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80"],
       subcategoryId: "tops",
       price: 42.0,
       discount: 0.0,
@@ -259,17 +262,17 @@ enum SampleData {
         StockItem(variant: ProductVariant(color: "burgundy", size: "L"), level: .low),
         StockItem(variant: ProductVariant(color: "black", size: "S"), level: .low),
         StockItem(variant: ProductVariant(color: "black", size: "L"), level: .normal),
-        StockItem(variant: ProductVariant(color: "navy blue", size: "M"), level: .low),
-        StockItem(variant: ProductVariant(color: "navy blue", size: "L"), level: .normal)
+        StockItem(variant: ProductVariant(color: "beige", size: "M"), level: .low),
+        StockItem(variant: ProductVariant(color: "beige", size: "L"), level: .normal)
       ],
       sizes: ["S", "M", "L"],
-      colors: ["burgundy", "black", "navy blue"],
+      colors: ["burgundy", "black", "beige"],
       overview: "Elegant wrap blouse made of shiny satin fabric. V-neckline and long sleeves with buttoned cuffs. Wrap design with self-tie belt.",
       tags: []
     ),
     Product(
       title: "Cropped hoodie",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1600348077475-d4db860d06f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"],
       subcategoryId: "tops",
       price: 45.0,
       discount: 0.0,
@@ -278,18 +281,18 @@ enum SampleData {
         StockItem(variant: ProductVariant(color: "white", size: "S"), level: .low),
         StockItem(variant: ProductVariant(color: "white", size: "M"), level: .low),
         StockItem(variant: ProductVariant(color: "white", size: "L"), level: .normal),
-        StockItem(variant: ProductVariant(color: "black", size: "XS"), level: .low),
-        StockItem(variant: ProductVariant(color: "black", size: "S"), level: .normal),
-        StockItem(variant: ProductVariant(color: "black", size: "L"), level: .low)
+        StockItem(variant: ProductVariant(color: "steel blue", size: "XS"), level: .low),
+        StockItem(variant: ProductVariant(color: "steel blue", size: "S"), level: .normal),
+        StockItem(variant: ProductVariant(color: "steel blue", size: "L"), level: .low)
       ],
       sizes: ["XS", "S", "M", "L"],
-      colors: ["white", "black"],
+      colors: ["white", "steel blue"],
       overview: "Cropped hoodie made of soft cotton blend fabric. Drawstring hood, long sleeves with dropped shoulders, and ribbed cuffs. Front pouch pocket, raw hem. Casual and comfortable design.",
       tags: ["new"]
     ),
     Product(
       title: "Cropped denim jacket",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1485811843447-c0989c7b9aae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"],
       subcategoryId: "coats",
       price: 69.0,
       discount: 0.10,
@@ -307,7 +310,7 @@ enum SampleData {
     ),
     Product(
       title: "Wide-leg trousers",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1604136172384-b2e9c43271ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"],
       subcategoryId: "trousers",
       price: 42.0,
       discount: 0.0,
@@ -325,7 +328,7 @@ enum SampleData {
     ),
     Product(
       title: "Boho midi dress",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1490505658643-e96d613eb642?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"],
       subcategoryId: "dresses",
       price: 65.0,
       discount: 0.0,
@@ -342,19 +345,19 @@ enum SampleData {
     ),
     Product(
       title: "Leather crossbody bag",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1505892458649-41be8480bb4a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"],
       subcategoryId: "accessories",
       price: 89.0,
       discount: 0.0,
       stock: [
-        StockItem(variant: ProductVariant(color: "brown", size: "One size"), level: .normal),
+        StockItem(variant: ProductVariant(color: "white", size: "One size"), level: .normal),
         StockItem(variant: ProductVariant(color: "black", size: "One size"), level: .low)
       ],
-      colors: ["brown", "black"]
+      colors: ["white", "black"]
     ),
     Product(
       title: "Wrap front midi dress",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1494578379344-d6c710782a3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"],
       subcategoryId: "dresses",
       price: 45.0,
       discount: 0.1,
@@ -362,17 +365,19 @@ enum SampleData {
         StockItem(variant: ProductVariant(color: "black", size: "S"), level: .normal),
         StockItem(variant: ProductVariant(color: "black", size: "M"), level: .low),
         StockItem(variant: ProductVariant(color: "black", size: "L"), level: .normal),
+        StockItem(variant: ProductVariant(color: "baby pink", size: "M"), level: .normal),
+        StockItem(variant: ProductVariant(color: "baby pink", size: "L"), level: .normal),
         StockItem(variant: ProductVariant(color: "red", size: "S"), level: .low),
         StockItem(variant: ProductVariant(color: "red", size: "L"), level: .normal)
       ],
       sizes: ["S", "M", "L"],
-      colors: ["black", "red"],
+      colors: ["black", "red", "baby pink"],
       overview: "Midi dress made of a soft, lightweight fabric. Wrap front design with v-neckline and short sleeves. Elasticated waist with tie belt. Flowy A-line skirt. Hits at mid-calf length.",
       tags: []
     ),
     Product(
       title: "Denim Shorts",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1585145197082-dba095ba01ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=702&q=80"],
       subcategoryId: "trousers",
       price: 29.99,
       discount: 0.0,
@@ -391,7 +396,7 @@ enum SampleData {
     ),
     Product(
       title: "Denim Jacket with Fleece Collar",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1521073535089-c3fe0eae04cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"],
       subcategoryId: "coats",
       price: 79.0,
       discount: 0.15,
@@ -411,7 +416,7 @@ enum SampleData {
     ),
     Product(
       title: "Cropped mom jeans with embroidery",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1510734790177-c931e3956547?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"],
       subcategoryId: "trousers",
       price: 45.0,
       discount: 0.0,
@@ -430,7 +435,7 @@ enum SampleData {
     ),
     Product(
       title: "Cotton shirt with pockets",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1517231426114-1ed5cdbe7248?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"],
       subcategoryId: "t-shirts",
       price: 39.0,
       discount: 0.0,
@@ -450,26 +455,26 @@ enum SampleData {
     ),
     Product(
       title: "Knit sweater with geometric pattern",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1527424803383-098895227b9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80"],
       subcategoryId: "tops",
       price: 59.99,
       discount: 0.0,
       stock: [
-        StockItem(variant: ProductVariant(color: "gray", size: "S"), level: .normal),
-        StockItem(variant: ProductVariant(color: "gray", size: "M"), level: .low),
-        StockItem(variant: ProductVariant(color: "gray", size: "L"), level: .normal),
+        StockItem(variant: ProductVariant(color: "green", size: "S"), level: .normal),
+        StockItem(variant: ProductVariant(color: "green", size: "M"), level: .low),
+        StockItem(variant: ProductVariant(color: "green", size: "L"), level: .normal),
         StockItem(variant: ProductVariant(color: "blue", size: "S"), level: .normal),
         StockItem(variant: ProductVariant(color: "blue", size: "M"), level: .normal),
         StockItem(variant: ProductVariant(color: "blue", size: "L"), level: .normal)
       ],
       sizes: ["S", "M", "L"],
-      colors: ["gray", "blue"],
+      colors: ["green", "blue"],
       overview: "Knit sweater with a geometric pattern in a relaxed fit. Rib-knit neckline, cuffs and hem.",
       tags: ["new"]
     ),
     Product(
       title: "Cotton blend sweater",
-      imageUrls: [],
+      imageUrls: ["https://images.unsplash.com/photo-1589697547048-962940abc062?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"],
       subcategoryId: "tops",
       price: 49.0,
       discount: 0.0,
